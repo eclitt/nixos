19ec8a3645309be7d36e759c151c5156b799043f
eclitt <vladtutrin095@gmail.com>
2025-09-27 20:54:48 +0700

    123

--- home-manager/modules/hyprland/hyprpaper.nix
+++ home-manager/modules/hyprland/hyprpaper.nix
@@ -4,10 +4,10 @@
     enable = true;
     settings = {
       ipc = "on";
-      preload = ["$HOME/Pictures/Wallpapers/normal/14.jpg"];
+      preload = ["$HOME/Pictures/Wallpapers/normal/3.jpg"];
       wallpaper = [
         ",$HOME/Pictures/Wallpapers/normal/14.jpg"
-        "HDMI-A-1,cover:$HOME/Pictures/Wallpapers/normal/14.jpg"
+        "HDMI-A-1,cover:$HOME/Pictures/Wallpapers/normal/3.jpg"
       ];
     };
   };
